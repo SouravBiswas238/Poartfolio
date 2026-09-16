497398a6289cd8eeea1389608571db4114d592e2
SouravBiswas238 <souravbiswas427@gmail.com>
2026-09-16 13:13:36 +0600

    update with tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -12,5 +13,6 @@ document.head.appendChild(link);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
